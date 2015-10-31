0bb1548adaeb2e51cf46b7b679925d41e09e3dc1
Muaad <muadh24@gmail.com>
2015-10-31 18:04:07 +0300

    Set up relationship between Driver and Truck

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,8 @@
 class Driver < ActiveRecord::Base
+	has_many :truck_drivers
+	has_many :trucks, through: :truck_drivers
+
+	def truck
+		trucks.where(active: true).first
+	end
 end
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,3 +1,9 @@
 class Truck < ActiveRecord::Base
 	belongs_to :truck_owner
+	has_many :truck_drivers
+	has_many :drivers, through: :truck_drivers
+
+	def driver
+		drivers.where(active: true).first
+	end
 end
