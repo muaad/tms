6ba186d7d72ca7909cf688068397eb4923a6558f
Muaad <muadh24@gmail.com>
2015-10-31 14:19:31 +0300

    Generated scaffold for ExpenseCategory

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :expense_categories
   resources :accounts
   root to: 'visitors#index'
   devise_for :users
---
+++ spec/factories/expense_categories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :expense_category do
+    name "MyString"
+  end
+
+end
