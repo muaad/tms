ff396eb468dee7611fefde7d1931ca73527dc99f
Muaad <muadh24@gmail.com>
2015-10-31 19:18:58 +0300

    Generated scaffold for Product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   resources :trucks
   resources :turn_boys
   resources :drivers
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031150513) do
+ActiveRecord::Schema.define(version: 20151031161801) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20151031150513) do
 
   create_table "expense_categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
