eeed2f66d4aa738c5889a21da01d9ff579dd0582
Muaad <muadh24@gmail.com>
2015-11-03 22:12:32 +0300

    Added product_type to Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031181757) do
+ActiveRecord::Schema.define(version: 20151103190921) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -80,8 +80,9 @@ ActiveRecord::Schema.define(version: 20151031181757) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.float    "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "product_type"
   end
 
   create_table "truck_drivers", force: :cascade do |t|
