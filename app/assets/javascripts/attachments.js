e79749f6f46a3577f79d907f57d2bd6f1e9262bc
Muaad <muadh24@gmail.com>
2015-11-22 21:49:58 +0300

    Added some JS for Attachment

---
+++ app/assets/javascripts/attachments.js
@@ -0,0 +1,34 @@
+function switchOwners(ownerType, ownerID) {
+	var owner = ownerType.slice(0,-1);
+	var lbl = '<label id="owner_type_label" for="attachment_' + owner + '_id">' + $('#attachment_type :selected').html() + '</label>'
+	var slct = '<select class="form-control" id="attachment_owner_list" name="attachment[' + owner + '_id]"></select>'
+	$('#attachment_owner').html(lbl + slct)
+	$.get('/' + ownerType + '.json', function(data) {
+		for (var i = 0; i < data.length; i++) {
+			if (ownerType === "trucks") {
+				var name = data[i].registration_number;
+			} else{
+				var name = data[i].name;
+			};
+			$('#attachment_owner_list').append('<option value=' + data[i].id + '>' + name + '</option>')
+		};
+	})
+	if (ownerID != 0) {
+		$('#attachment_type option[value="'+ ownerType +'"]').attr('selected', 'selected');
+		$('#attachment_owner_list option[value="'+ ownerID +'"]').attr('selected', 'selected');
+		$('#owner_type_label').html($('#attachment_type :selected').html())
+	};
+}
+
+$(function() {
+	var editPage = window.location.pathname.indexOf("edit") >= 0 && window.location.pathname.indexOf("attachments") >= 0
+	if (editPage) {
+		switchOwners($('#ownerType').val(), $('#ownerID').val());
+		$('#attachment_owner_list option[value="'+ $('#ownerID').val() +'"]').attr('selected', 'selected');
+	}
+	if (window.location.pathname === "/attachments/new") {
+		$('#attachment_type').change(function() {
+			switchOwners($('#attachment_type').val(), 0);
+		})
+	}
+});
