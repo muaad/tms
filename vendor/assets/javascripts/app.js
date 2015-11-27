42b2cae5f9986cce7ed7093cdea6df97dd237896
Muaad <muadh24@gmail.com>
2015-11-27 15:56:48 +0300

    Doing the homepage

--- vendor/assets/javascripts/app.js
+++ vendor/assets/javascripts/app.js
@@ -255,14 +255,14 @@ function _init() {
       //Set the min-height of the content and sidebar based on the
       //the height of the document.
       if ($("body").hasClass("fixed")) {
-        $(".content-wrapper, .right-side").css('min-height', window_height - $('.main-footer').outerHeight());
+        $(".content-wrapper, .content_wrapper, .right-side").css('min-height', window_height - $('.main-footer').outerHeight());
       } else {
         var postSetWidth;
         if (window_height >= sidebar_height) {
-          $(".content-wrapper, .right-side").css('min-height', window_height - neg);
+          $(".content-wrapper, .content_wrapper, .right-side").css('min-height', window_height - neg);
           postSetWidth = window_height - neg;
         } else {
-          $(".content-wrapper, .right-side").css('min-height', sidebar_height);
+          $(".content-wrapper, .content_wrapper, .right-side").css('min-height', sidebar_height);
           postSetWidth = sidebar_height;
         }
 
@@ -270,7 +270,7 @@ function _init() {
         var controlSidebar = $($.AdminLTE.options.controlSidebarOptions.selector);
         if (typeof controlSidebar !== "undefined") {
           if (controlSidebar.height() > postSetWidth)
-            $(".content-wrapper, .right-side").css('min-height', controlSidebar.height());
+            $(".content-wrapper, .content_wrapper, .right-side").css('min-height', controlSidebar.height());
         }
 
       }
