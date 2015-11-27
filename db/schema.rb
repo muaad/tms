8d59be980ff46bc086b875741d5a380b1e2cfe05
Muaad <muadh24@gmail.com>
2015-11-27 18:07:31 +0300

    Added token to Account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127150424) do
+ActiveRecord::Schema.define(version: 20151127150549) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151127150424) do
     t.boolean  "active",       default: false
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "token"
   end
 
   create_table "attachments", force: :cascade do |t|
