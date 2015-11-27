22cbd92bb769cf1b28a794e5122ad731e1abed34
Muaad <muadh24@gmail.com>
2015-11-27 18:05:09 +0300

    Added is_admin to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127143908) do
+ActiveRecord::Schema.define(version: 20151127150424) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -244,20 +244,21 @@ ActiveRecord::Schema.define(version: 20151127143908) do
   add_index "turn_boys", ["account_id"], name: "index_turn_boys_on_account_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "name"
     t.integer  "account_id"
+    t.boolean  "is_admin",               default: false
   end
 
   add_index "users", ["account_id"], name: "index_users_on_account_id"
