55c6a68efaae15d68b4dcbe87bbfeba0d98c1b6c
Muaad <muadh24@gmail.com>
2015-11-28 15:42:19 +0300

    Seeds for ExpenseCategory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+ExpenseCategory.find_or_create_by! name: "Spare parts"
+ExpenseCategory.find_or_create_by! name: "Driver Salary"
+ExpenseCategory.find_or_create_by! name: "TurnBoy Salary"
+ExpenseCategory.find_or_create_by! name: "Diesel"
