4bfbeeade2b914ec9fb9fbdd85569ebb8de0c486
Muaad <muadh24@gmail.com>
2016-03-05 20:43:27 +0300

    Replaced scopes with class methods in the Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,12 +2,28 @@ class Notification < ActiveRecord::Base
 	attr_accessor :attachment_name
   belongs_to :attachment
 
-  scope :read, -> { where('read = ?', true) }
-  scope :resolved, -> { where('resolved = ?', true) }
-  scope :unread, -> { where('read = ?', false) }
-  scope :unresolved, -> { where('resolved = ?', false) }
+  # scope :read, -> { where('read = ?', true) }
+  # scope :resolved, -> { where('resolved = ?', true) }
+  # scope :unread, -> { where('read = ?', false) }
+  # scope :unresolved, -> { where('resolved = ?', false) }
 
   def attachment_and_owner
   	"#{attachment.name} - #{attachment.owner_name}"
   end
+
+  def self.read
+	  Notification.where(read: true)
+	end
+
+	def self.unread
+	  Notification.where(read: false)
+	end
+
+	def self.resolved
+	  Notification.where(resolved: true)
+	end
+
+	def self.unresolved
+	  Notification.where(resolved: false)
+	end
 end
